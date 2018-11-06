3e413b4dafb181c8758e4f154631f920456361d3
VioletStair <violetstair@gmail.com>
2018-11-06 14:04:17 +0900

    Update: init value

--- owdinnetwork/controller/config/owdin_config.hpp
+++ owdinnetwork/controller/config/owdin_config.hpp
@@ -11,7 +11,6 @@ namespace owdin {
         private:
             account_name self;
 
-
         public:
             owdin_config( account_name _self ): self( _self ) { };
 
@@ -74,7 +73,7 @@ namespace owdin {
             void initial( account_name account, uint8_t object_type ) {
                 time current_time = now();
                 configsIndex configs( self, account );
-
+                string data = "";
                 auto itr = configs.find( account );
                 eosio_assert( itr != configs.end(), "can not found account" );
 
@@ -84,16 +83,16 @@ namespace owdin {
                     s.sys_receiver = self;
                     s.net_receiver = self;
                     s.sec_receiver = self;
-                    s.ipfs_conf = "";
-                    s.proc_conf = "";
-                    s.ipfs_hash = "";
-                    s.proc_hash = "";
-                    s.sys_conf = "";
-                    s.sys_hash = "";
-                    s.net_conf = "";
-                    s.net_hash = "";
-                    s.sec_conf = "";
-                    s.sec_hash = "";
+                    s.ipfs_conf = data;
+                    s.proc_conf = data;
+                    s.ipfs_hash = data;
+                    s.proc_hash = data;
+                    s.sys_conf = data;
+                    s.sys_hash = data;
+                    s.net_conf = data;
+                    s.net_hash = data;
+                    s.sec_conf = data;
+                    s.sec_hash = data;
                     s.ipfs_update = current_time;
                     s.proc_update = current_time;
                     s.sys_update = current_time;
@@ -104,6 +103,7 @@ namespace owdin {
             }
 
             void clear( account_name account, uint8_t object_type ) {
+                string data = "";
                 time current_time = now();
                 configsIndex configs( self, account );
 
@@ -113,36 +113,36 @@ namespace owdin {
                 if (object_type == OBJECT_STORAGE ) {
                     configs.modify( itr, self, [&]( auto& s ) {
                         s.ipfs_receiver = self;
-                        s.ipfs_conf = "";
-                        s.ipfs_hash = "";
+                        s.ipfs_conf = data;
+                        s.ipfs_hash = data;
                         s.ipfs_update = current_time;
                     });
                 } else if (object_type == OBJECT_NETWORK ) {
                     configs.modify( itr, self, [&]( auto& s ) {
                         s.net_receiver = self;
-                        s.net_conf = "";
-                        s.net_hash = "";
+                        s.net_conf = data;
+                        s.net_hash = data;
                         s.net_update = current_time;
                     });
                 } else if (object_type == OBJECT_SYSTEM ) {
                     configs.modify( itr, self, [&]( auto& s ) {
                         s.sys_receiver = self;
-                        s.sys_conf = "";
-                        s.sys_hash = "";
+                        s.sys_conf = data;
+                        s.sys_hash = data;
                         s.sys_update = current_time;
                     });
                 } else if (object_type == OBJECT_PROCESS ) {
                     configs.modify( itr, self, [&]( auto& s ) {
                         s.proc_receiver = self;
-                        s.proc_conf = "";
-                        s.proc_hash = "";
+                        s.proc_conf = data;
+                        s.proc_hash = data;
                         s.proc_update = current_time;
                     });
                 } else if (object_type == OBJECT_SECURITY ) {
                     configs.modify( itr, self, [&]( auto& s ) {
                         s.sec_receiver = self;
-                        s.sec_conf = "";
-                        s.sec_hash = "";
+                        s.sec_conf = data;
+                        s.sec_hash = data;
                         s.sec_update = current_time;
                     });
                 } else {
